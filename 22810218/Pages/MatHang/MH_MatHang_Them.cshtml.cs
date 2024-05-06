fabe46b23aff702702460c8556fbe9f5bd6baebe
CaoQuocViet <vitthongthai02@gmail.com>
2024-05-06 13:48:00 +0700

    Fixed MatHang

--- 22810218/Pages/MatHang/MH_MatHang_Them.cshtml.cs
+++ 22810218/Pages/MatHang/MH_MatHang_Them.cshtml.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Entities;
@@ -9,13 +8,49 @@ namespace Pages
     public class MH_MatHang_ThemModel : PageModel
     {
         [BindProperty]
-        public MatHang? Mathang { get; set; }
-
-        public string? SInfo { get; set; }
+        public MatHang Mathang { get; set; }
+        public string SInfo { get; set; }
+        public bool BFlag { get; set; }
 
         public void OnGet()
         {
+            ViewData["Title"] = "Quản lý cửa hàng";
+            Mathang = new MatHang("", "", "", "", 0);
         }
 
+        public IActionResult OnPost()
+        {
+            BFlag = true;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            string sMaSo = Request.Form["id"];
+            string sTen = Request.Form["name"];
+            string sLoaiHang = Request.Form["loaihang"];
+            string sThuongHieu = Request.Form["brand"];
+            string sGia = Request.Form["price"];
+
+            MatHang tempMatHang = new MatHang(sMaSo, sTen, sLoaiHang, sThuongHieu, int.Parse(sGia));
+
+            int parsedGia;
+            if (!int.TryParse(sGia, out parsedGia))
+            {
+                SInfo = "Giá mặt hàng không hợp lệ";
+                return Page();
+            }
+            tempMatHang.Gia = parsedGia;
+
+            XL_MatHang xlMatHang = new XL_MatHang();
+            SInfo = xlMatHang.Them(sMaSo, sTen, sLoaiHang, sGia, sThuongHieu, ref tempMatHang);
+
+            if (string.IsNullOrEmpty(SInfo))
+            {
+                Mathang = tempMatHang;
+            }
+
+            return Page();
+        }
     }
 }
