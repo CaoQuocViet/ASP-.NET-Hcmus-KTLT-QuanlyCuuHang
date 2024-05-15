d69c716dd7993a730f47ef9416dfc48b7e150fc2
CaoQuocViet <vitthongthai02@gmail.com>
2024-05-15 09:22:05 +0700

    Fixed DATA

--- Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7028e13f4213444b47491ec38b594abb47cbea3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f4b31c68df6b4b0ef67a1377ca9f915d945b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
